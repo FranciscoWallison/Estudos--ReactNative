7cda38fb2a39d5991dfc6c4b709ca3cd8aac79ca
Wallison <wallison.nascimento@odontosystem.com.br>
2019-11-06 18:07:08 -0300

    Entendendo PROPS

--- React Native Basico/PrimeiroProjeto/App.js
+++ React Native Basico/PrimeiroProjeto/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class PrimeiroProjeto extends Component {
 
   render(){
+
+    let imagem = {
+      uri:'https://avatars3.githubusercontent.com/u/19413241?s=460&v=4' 
+    };
+
    return (
      <View>
        <Text>Olá Mundo</Text>
        <Text>Olá Mundo</Text>
        <Text>Olá Mundo</Text>
+       <Text style={{fontSize:25, color:'red', margin:20}}>Olá Mundo</Text>
+       <Image source={imagem} style={{ width:300, height:300 }}/>
      </View>
      
    ); 

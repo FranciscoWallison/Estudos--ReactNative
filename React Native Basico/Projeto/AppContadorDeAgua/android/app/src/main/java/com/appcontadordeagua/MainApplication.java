8c62ff4a4e50f10cb7230751fb25a2be34ced71b
Wallison <wallison.nascimento@odontosystem.com.br>
2019-11-11 15:23:27 -0300

    Projeto - App Contador de Água

--- React Native Basico/Projeto/AppContadorDeAgua/android/app/src/main/java/com/appcontadordeagua/MainApplication.java
+++ React Native Basico/Projeto/AppContadorDeAgua/android/app/src/main/java/com/appcontadordeagua/MainApplication.java
@@ -1,4 +1,4 @@
-package com.primeiroprojeto;
+package com.appcontadordeagua;
 
 import android.app.Application;
 import android.content.Context;
--- React Native Basico/Projeto/AppContadorDeAgua/ios/AppContadorDeAguaTests/AppContadorDeAguaTests.m
+++ React Native Basico/Projeto/AppContadorDeAgua/ios/AppContadorDeAguaTests/AppContadorDeAguaTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PrimeiroProjetoTests : XCTestCase
+@interface AppContadorDeAguaTests : XCTestCase
 
 @end
 
-@implementation PrimeiroProjetoTests
+@implementation AppContadorDeAguaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
